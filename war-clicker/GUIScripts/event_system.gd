711d76d83489cb10d2d618da8c58268ae2379150
p0rtcullis <p0rtcullis@protonmail.com>
2026-05-26 15:51:39 -0500

    Events4000

--- war-clicker/GUIScripts/event_system.gd
+++ war-clicker/GUIScripts/event_system.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var trigger_turns : Array = [2,5,8,10]
+@onready var canvas_layer : CanvasLayer = %CanvasLayer
 
 var event_lookup_table : Dictionary = {2:_scout_encounter}
 	
@@ -13,7 +14,11 @@ func _ready():
 #subject to change.
 func _on_event_triggered(lookup_number):
 	print(lookup_number)
-	event_lookup_table[lookup_number].call()
+	#event_lookup_table[lookup_number].call()
+	var question_box = preload("res://question_box.tscn")
+	var question = question_box.instantiate()
+	canvas_layer.add_child(question)
+	
 	
 func _scout_encounter():
 	print("Scouts looking for their missing comrade are dangerously close to base. A show of force could lead to the capture of the entire squad, at risk of raising an alarm. Alternatively, they could be redirected by sacrificing a few lesser bioforms.")
---
+++ war-clicker/question_box.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+func _on_button_pressed() -> void:
+	print("Option1 Picked")
+	
+func _on_button_2_pressed() -> void:
+	print("option2 Picked")
