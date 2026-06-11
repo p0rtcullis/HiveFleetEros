b9f1e77bdad45781959d749d85d3fec93e039333
p0rtcullis <p0rtcullis@protonmail.com>
2026-06-11 15:57:30 -0500

    Major Update

--- war-clicker/GUIScripts/production_screen_gui.gd
+++ war-clicker/GUIScripts/production_screen_gui.gd
@@ -7,7 +7,11 @@ extends Control
 @onready var ripple_template
 @onready var psy_template
 
-@onready var selected_worker = %Ranger
+@onready var selected_worker = %Ranger:
+	set(value):
+		selected_worker = value
+		%SelectedWorkerLabel.text = "Selected Worker: " + selected_worker.stats.worker_name
+		
 
 var prod_mod : int = 0
 @onready var unlocked_units :Array = [stabber_template]
@@ -20,6 +24,7 @@ func _ready() -> void:
 func _end_turn_production():
 	_worker_production()
 	_production_update()
+	_display_queue(selected_worker)
 	
 func _unit_unlocked(unit):
 	unlocked_units.append(unit_types[unit])
@@ -35,43 +40,51 @@ func _worker_production():
 func _production_update() -> void:
 	for worker in %ManagementScreen.worker_list:
 		if worker.production_queue.is_empty():
-			#print("Nothing to Produce!")
 			continue
 		else:
-			if worker.stats.production_timer < worker.production_queue[0].stats.cook_time:
-				#print("Turns Remaining: " + str(worker.production_queue[0].stats.cook_time - worker.stats.production_timer))
+			if worker.stats.production_timer < worker.production_queue[0].stats.cook_time-1:
 				worker.stats.production_timer +=1
 			else:
-				#print("Unit Produced")
 				worker.stats.production_timer = 0
 				Events.unit_gain.emit(worker.production_queue.pop_front())
-				#print(%ManagementScreen.unit_list)
+				
+func _create_unit(template):
+	if template not in unlocked_units:
+		print("Upgrade not unlocked!")
+	else:
+		var new_unit = template.instantiate()
+		selected_worker.production_queue.append(new_unit)
+		_display_queue(selected_worker)
+
+func _display_queue(worker):
+	%ProductionQueue.clear()
+	var index = 0
+	for unit in worker.production_queue:
+		if index == 0:
+			%ProductionQueue.add_item(unit.stats.unit_name + " turns remaining: " + str(unit.stats.cook_time-worker.stats.production_timer))
+		else:
+			%ProductionQueue.add_item(unit.stats.unit_name + " turns remaining: " + str(unit.stats.cook_time+unit.stats.cook_time-worker.stats.production_timer+index-1))
+		index +=1
+		
+func _on_production_queue_item_activated(index: int) -> void:
+	selected_worker.production_queue.pop_at(index)
+	_display_queue(selected_worker)
 
 func _on_ranger_prodction_button_pressed() -> void:
 	selected_worker = %Ranger
+	_display_queue(%Ranger)
 	
 func _on_spider_production_button_pressed() -> void:
 	selected_worker = %Spider
-
+	_display_queue(%Spider)
 #Shooter
 func _on_shooter_production_button_pressed() -> void:
-	if shooter_template not in unlocked_units:
-		print("Upgrade not unlocked!")
-	else:
-		var unit = shooter_template.instantiate()
-		selected_worker.production_queue.append(unit)
+	_create_unit(shooter_template)
 
 #Stabber
 func _on_stabber_production_button_pressed() -> void:
-	if stabber_template not in unlocked_units:
-		print("Upgrade not unlocked!")
-	else:
-		var unit = stabber_template.instantiate()
-		selected_worker.production_queue.append(unit)
+	_create_unit(stabber_template)
 
 
 func _on_warrior_production_button_pressed() -> void:
-	if warrior_template not in unlocked_units:
-		print("Upgrade not unlocked!")
-	else:
-		var unit = warrior_template.instantiate()
+	_create_unit(warrior_template)
